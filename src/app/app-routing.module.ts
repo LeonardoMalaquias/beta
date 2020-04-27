af611e25e515caa0fbc2e9c9367bbad4c8b5b70d
Leonardo Malaquias <leonardocesar91@gmail.com>
2020-04-27 00:25:42 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientFormComponent } from './client-form/client-form.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'cliente', component: ClientFormComponent },
+	{ path: 'usuario', component: UserFormComponent },
+	{ path: 'menu', component: NavbarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ClientFormComponent } from './client-form/client-form.component';
+import { UserFormComponent } from './user-form/user-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientFormComponent,
+    UserFormComponent,
+    NavbarComponent
   ],
   imports: [
+    MatIconModule,
+    MatSliderModule,
+    MatMenuModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
